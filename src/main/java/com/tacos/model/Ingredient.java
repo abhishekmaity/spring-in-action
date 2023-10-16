db8ac203ec397fc305a49c101d22b37628c5055a
Abhishek Maity <abhishek.maity@hotmail.com>
2023-10-17 01:56:40 +0530

    continue add c-2

---
+++ src/main/java/com/tacos/model/Ingredient.java
@@ -0,0 +1,37 @@
+package com.tacos.model;
+
+public class Ingredient {
+	private final String id;
+	private final String name;
+	private final Type type;
+
+	public enum Type {
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
+
+	public Ingredient(String id, String name, Type type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	@Override
+	public String toString() {
+		return "Ingredient [id=" + id + ", name=" + name + ", type=" + type + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/tacos/model/Taco.java
@@ -0,0 +1,20 @@
+package com.tacos.model;
+
+import java.util.List;
+
+public class Taco {
+	private String name;
+	private List<Ingredient> ingredients;
+	public List<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(List<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
