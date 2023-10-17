c7055d964042f196d9690144fcf1a2218002616f
Abhishek Maity <abhishek.maity@hotmail.com>
2023-10-17 23:18:35 +0530

    c-2

--- src/main/java/com/tacos/controller/OrderController.java
+++ src/main/java/com/tacos/controller/OrderController.java
@@ -1,7 +1,7 @@
 package com.tacos.controller;
 
-
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +10,25 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import com.tacos.model.TacoOrder;
 
+import jakarta.validation.Valid;
+
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
-	
+
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(OrderController.class);
-	
+
 	@GetMapping("/current")
 	public String orderForm() {
 		return "orderForm";
 	}
 
 	@PostMapping
-	public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+	public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
 		log.info("Order submitted: {}", order);
 		sessionStatus.setComplete();
 		return "redirect:/";
