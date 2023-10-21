1aea9d7908dc024edebb0b006171afe696089b15
Abhishek Maity <abhishek.maity@hotmail.com>
2023-10-21 14:36:19 +0530

    patch-13

--- src/main/java/com/tacos/repository/OrderRepository.java
+++ src/main/java/com/tacos/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.tacos.repository;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -9,5 +10,8 @@ import com.tacos.model.TacoOrder;
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 	TacoOrder save(TacoOrder order);
 	
-	List<TacoOrder> findByDeliveryZip(String deliveryZip);
+//	List<TacoOrder> findByDeliveryZip(String deliveryZip);
+	
+	List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+			String deliveryZip, Date startDate, Date endDate);
 }
