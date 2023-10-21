ed24493509e42e14007a6e1b36599e38012d023a
Abhishek Maity <abhishek.maity@hotmail.com>
2023-10-21 14:23:59 +0530

    patch-11

--- src/main/java/com/tacos/repository/IngredientRepository.java
+++ src/main/java/com/tacos/repository/IngredientRepository.java
@@ -2,9 +2,13 @@ package com.tacos.repository;
 
 import java.util.Optional;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tacos.model.Ingredient;
+import com.tacos.model.Ingredient.Type;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 	Iterable<Ingredient> findAll();
@@ -12,4 +16,38 @@ public interface IngredientRepository extends CrudRepository<Ingredient, String>
 	Optional<Ingredient> findById(String id);
 
 	Ingredient save(Ingredient ingredient);
+
+//	@Bean
+//	default CommandLineRunner dataLoader(IngredientRepository repo) {
+//		return args -> {
+//			repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+//			repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+//			repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+//			repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+//			repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+//			repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+//			repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+//			repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+//			repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+//			repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+//		};
+//	}
+
+//	@Bean
+//	default ApplicationRunner dataLoader(IngredientRepository repo) {
+//		return args -> {
+//			List<String> version = args.getOptionValues("version");
+	
+//			repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+//			repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+//			repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+//			repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+//			repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+//			repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+//			repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+//			repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+//			repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+//			repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+//		};
+//	}
 }
